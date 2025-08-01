2a9edbbcdf1446cc61ed37c2f398a4d350f28f4d
Joaomagh <joaomagdev@gmail.com>
2025-07-31 22:43:09 -0300

    deploy backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,19 @@ const participantRoutes = require('./routes/participantRoutes');
 const app = express();
 
 // Middlewares
+// Configuração do CORS - Permite requisições APENAS do seu front-end na Vercel
 app.use(cors({
-  origin: 'https://cotabox-challenge-frontend.vercel.app/' 
+  origin: 'https://cotabox-challenge-frontend-md3m81hr2.vercel.app' 
 }));
 app.use(express.json());
 
 // Rotas
 app.use('/api/participants', participantRoutes);
+
+// Nova rota para o caminho /api
+app.get('/api', (req, res) => {
+    res.send('Bem-vindo à API de Participantes!');
+});
 
 app.get('/', (req, res) => {
     res.send('API funcionando!');
